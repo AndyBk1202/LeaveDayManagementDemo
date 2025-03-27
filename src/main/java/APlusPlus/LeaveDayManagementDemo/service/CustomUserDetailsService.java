bca130e5440350e0fa627e4c786c0a076d66fb43
Andy <andybk.nguyen1202@gmail.com>
2025-03-27 16:00:42 +0700

    add UserDetails and UserDetailsService

--- src/main/java/APlusPlus/LeaveDayManagementDemo/service/CustomUserDetailsService.java
+++ src/main/java/APlusPlus/LeaveDayManagementDemo/service/CustomUserDetailsService.java
@@ -1,2 +1,19 @@
-package APlusPlus.LeaveDayManagementDemo.service;public class CustomUserDetailsService {
+package APlusPlus.LeaveDayManagementDemo.service;
+
+import APlusPlus.LeaveDayManagementDemo.exception.OurException;
+import APlusPlus.LeaveDayManagementDemo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepo.findByEmail(username).orElseThrow(() -> new OurException("Username not found"));
+    }
 }
